d9666f6e3fa9d89005fc9b6fa5853abb89bcbb8a
chemakhi imad <chemakhi.imad@gmail.com>
2024-11-23 19:33:38 +0100

    karma configuratio file modification 2

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,26 @@
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
-      // ... other configurations ...
-      
-      browsers: ['ChromeHeadless'],
-      
-      customLaunchers: {
-        ChromeHeadless: {
-          base: 'Chrome',
-          flags: [
-            '--no-sandbox',
-            '--headless',
-            '--disable-gpu',
-            '--remote-debugging-port=9222',
-            '--disable-dev-shm-usage'
-          ]
-        }
-      },
-  
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        // ... other plugins ...
-      ]
+        // ... other configurations ...
+
+        browsers: ['ChromeHeadless'],
+
+        customLaunchers: {
+            ChromeHeadless: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    '--headless',
+                    '--disable-gpu',
+                    '--remote-debugging-port=9222',
+                    '--disable-dev-shm-usage'
+                ]
+            }
+        },
+
+        plugins: [
+            require('karma-chrome-launcher'),
+            require('karma-jasmine'),
+            require('karma-typescript')
+        ]
     });
-  };
+};
